dfb7134f48782455d1286c2df1262f207bb48b43
Viet Duc Kieu <viet.duc.kieu@web.de>
2023-03-20 16:59:50 +0100

    add framer motion

--- components/HeroTitle.tsx
+++ components/HeroTitle.tsx
@@ -1,16 +1,39 @@
 import React from "react"
 import { Title, Text, Avatar, Container } from '@mantine/core';
 import { IconBrandGithub, IconBrandLinkedin, IconMail } from '@tabler/icons-react';
+import { motion } from "framer-motion";
 
 const HeroTitle = () => {
     return (
         <Container mx={"auto"}>
-            <Avatar src={"duc.jpg"} size={120} alt="Duc" radius={60} mx={"auto"} />
-            <Title order={1}>Viet Duc Kieu</Title>
-            <Text>Business Information Systems Student</Text>
-            <IconBrandLinkedin size={30} />
-            <IconBrandGithub size={30} />
-            <IconMail size={30} />
+            <motion.div
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8 }}
+            >
+                <Avatar src={"duc.jpg"} size={120} alt="Duc" radius={60} mx={"auto"} />
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 0.7, ease: "easeIn" }}
+            >
+                <Title order={1}>Viet Duc Kieu</Title>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 0.5, ease: "easeIn" }}
+            >
+                <Text>
+                    Business Information Systems Student
+                </Text>
+                <div>
+                    <IconBrandLinkedin size={30} />
+                    <IconBrandGithub size={30} />
+                    <IconMail size={30} />
+                </div>
+            </motion.div>
         </Container>
     )
 }
